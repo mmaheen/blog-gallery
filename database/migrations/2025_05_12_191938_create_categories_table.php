eb8b4454391c203aa002bd553153b015893a0641
mmaheen <mushrifmaheen@gmail.com>
2025-05-13 01:24:50 +0600

    Category migration

--- database/migrations/2025_05_12_191938_create_categories_table.php
+++ database/migrations/2025_05_12_191938_create_categories_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->unSignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
     }
 
